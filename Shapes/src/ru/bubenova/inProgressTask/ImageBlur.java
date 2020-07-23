95de3979d6916abeda4004f21facc5d1f6577261
jubenova <bubenova-yaya@mail.ru>
2020-07-23 20:10:55 +0700

    работаю над размытием

--- Shapes/src/ru/bubenova/inProgressTask/ImageBlur.java
+++ Shapes/src/ru/bubenova/inProgressTask/ImageBlur.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 
 public class ImageBlur {
     public static void main(String[] args) throws IOException {
-        // читаем картинку из файлу image.jpg в объект класса BufferedImage
+ /*       // читаем картинку из файлу image.jpg в объект класса BufferedImage
         BufferedImage image = ImageIO.read(new File("image.jpg"));
 
         // получаем растр - объект, внутри которого содержится двумерный массив пикселей
@@ -20,15 +20,36 @@ public class ImageBlur {
         int width = raster.getWidth();
         int height = raster.getHeight();
 
+        //создаем копию изображения, изменять будем его, беря значения из исходной картинки
+        BufferedImage tempImage = new BufferedImage(image.getWidth(), image.getHeight(), image.getType());
+
+        // получаем растр из скопированной картинки - объект, внутри которого содержится двумерный массив пикселей
+        WritableRaster tempRaster = tempImage.getRaster();
+*/
         // просим пользователя определить степень размытия
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите коэффициент размытия: ");
-        int blurCoeficient = scanner.nextInt();
+
+        int blurCoefficient;
+        int minBlurCoefficient = 3;
+
+        for (; ; ) {
+            System.out.println("Введите коэффициент размытия: ");
+            blurCoefficient = scanner.nextInt();
+
+            if (blurCoefficient > minBlurCoefficient || blurCoefficient % 2 != 0) {
+                break;
+            }
+
+            System.out.println("Коэффициент должен быть 1) нечетным; 2) больше или равен " + minBlurCoefficient);
+        }
+
+        System.out.println(blurCoefficient);
 
 
 
 
 
-        ImageIO.write(image, "png", new File("blur.png"));
+
+       // ImageIO.write(image, "png", new File("blur.png"));
     }
 }
